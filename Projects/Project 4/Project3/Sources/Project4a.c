e9787efcdc7c0d0abed4daba2a92ff6631f95e25
Kyle Ashley <kyle.ashley183@gmail.com>
2014-02-13 16:22:01 -0500

    added

--- Projects/Project 4/Project3/Sources/Project4a.c
+++ Projects/Project 4/Project3/Sources/Project4a.c
@@ -13,14 +13,16 @@
 volatile unsigned int mt_ovf = 0;
 unsigned int d;
 butterworth_2pole_ft butterF;
-long distance1;
+long distance1 = 0;
 int avgButter2 = 0;                             // fixed filter val
 unsigned char avgBufF[9] = {0};                 // filter output buffer
 
 const int Rstep[] = {0xC0, 0x60, 0x90, 0xA0};
 const int Lstep[] = {0x0C, 0x06, 0x09, 0x0A};
-int Lspeed = 50;
-int Rspeed = 50;
+int Lspeed = 1;
+int Rspeed = 1;
+
+static unsigned int Rmotcount, Lmotcount;
 
 void main(void) {
   /* put your own code here */
@@ -43,6 +45,10 @@ void main(void) {
   
   TSCR1 = 0x80;            // set up MT
   TSCR2 = 0x82;
+  
+   MCCTL = 0xEE;                       // Pre = 8
+  MCCNT = 300;                       // LOAD = 32
+ 
 
   atd0_powerOn();              // Powers on ATD module 0
   atd0_setFFC(1);              // set fast flag clearing on/off
@@ -58,14 +64,16 @@ void main(void) {
   
 	EnableInterrupts;
 	
-  lcd_setup();     // set up the LCD to write
+  //lcd_setup();     // set up the LCD to write
+  DDRB = 0xFF;
 
   for(;;) {
   
-    sprintf(avgBufF, "%3d", avgButter2);   
+  
+    //sprintf(avgBufF, "%3d", avgButter2);   
     //output the average and time on the LCD
-    lcd_clear();   
-    lcd_outputString(avgBufF);
+    //lcd_clear();   
+    //lcd_outputString(avgBufF);
     _FEED_COP(); /* feeds the dog */
   } /* loop forever */
   /* please make sure that you never leave main */
@@ -75,46 +83,77 @@ void main(void) {
 #pragma TRAP_PROC
 #pragma CODE_SEG __SHORT_SEG NON_BANKED
 interrupt VectorNumber_Vrti void rtiInterrupt () {
-  static unsigned int rti_count = 0;
-  long val;                                       // sensor read
+
+  static int on = 0;
+  long val; 
+  static unsigned int distcount;                                       // sensor read
   
   unsigned int i, j;
   unsigned int start, end;
-  int time_butter, time_butter2;
-  
-  static int rIndex = 0;
-  static int lIndex = 0;
-  static int Rmotcount, Lmotcount;
-  unsigned char Rout, Lout;
-  static unsigned int Lindex, Rindex;
- 
   
   // ALWAYS CLEAR THE FLAG FIRST!
   CRGFLG |= 0x80;
-  
-  rti_count++;
-  Rmotcount++;
-  Lmotcount++;
-  if (rti_count >= 100)   // every 100ms
+  distcount++;
+    
+  if (distcount >= 2)   // every 100ms
   {
     // reset the counters and timing variables
-    rti_count = 0;
+    distcount = 0;
     //start = 0;
     //end = 0;
 
     val = atd0_readChX(0);                        // read atd ch0
-    
-    mt_ovf = 0;
-    
+    //mt_ovf = 0;
     //start = TCNT;
     butterworth_2pole_putF(&butterF, val);        //compute fixed filter
     avgButter2 = butterworth_2pole_get_longF(&butterF);
     distance1=avgButter2;
     //end = TCNT;
-    Rspeed = 4;
-    Lspeed = 4;
-  }
-    
+
+   }
+}
+  
+
+// used for timing the averaging
+#pragma TRAP_PROC
+#pragma CODE_SEG __SHORT_SEG NON_BANKED
+interrupt VectorNumber_Vtimovf void ovfInterrupt () 
+{
+  
+  // ALWAYS CLEAR THE FLAG FIRST!
+  TFLG2 |= 0x80;
+  
+  mt_ovf++;          // count ovfs
+}
+
+#pragma TRAP_PROC
+#pragma CODE_SEG __SHORT_SEG NON_BANKED
+interrupt VectorNumber_Vtimmdcu void mdcuInterrupt () {
+  
+  static int rIndex = 0;
+  static int lIndex = 0;
+
+  unsigned char Rout, Lout;
+  static unsigned int Lindex, Rindex;
+  
+  // ALWAYS CLEAR THE FLAG FIRST!
+  MCFLG |= 0x80;
+  
+  Rmotcount++;
+  Lmotcount++;
+  
+  //  if(distance1 > 50)
+  //  {
+      
+      Rspeed = distance1 * 4;
+      Lspeed = distance1 * 4;
+  //  }
+  //      else
+   // {
+      
+  //    Rspeed = 9999999;
+  //    Lspeed = 9999999;
+   // }
   
   if(Rmotcount >= Rspeed || Rmotcount >= Lspeed)
   {
@@ -139,37 +178,9 @@ interrupt VectorNumber_Vrti void rtiInterrupt () {
       
     PORTB = Rout + Lout;
   }
-}
+  
+  
 
-// used for timing the averaging
-interrupt VectorNumber_Vtimovf void ovfInterrupt () 
-{
   
-  // ALWAYS CLEAR THE FLAG FIRST!
-  TFLG2 |= 0x80;
-  
-  mt_ovf++;          // count ovfs
 }
-/*
-#pragma TRAP_PROC
-#pragma CODE_SEG __SHORT_SEG NON_BANKED
-interrupt VectorNumber_Vtimmdcu void mdcuInterrupt () {
-  static int mdcu_count = 0;
-  static int on = 0;
-  
-  // ALWAYS CLEAR THE FLAG FIRST!
-  MCFLG |= 0x80;
-  
-  mdcu_count++;
-  if (mdcu_count >= 100) {
-    mdcu_count = 0;
-    if (on==1) {
-      on = 0;
-      PTH_PTH3 = 1;
-    } else {
-      on = 1;
-      PTH_PTH3 = 0;
-    }
-  }
-}
-    */
+   
